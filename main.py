c1dcd9e2b78369de7973590e13b3ddedb79e9f07
bintang edma <bintangdragneel@gmail.com>
2020-05-02 04:51:24 +0700

    menghapus main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import time
-import datetime
-from pymysql import *
-import Class.Administrator
-import Class.Employee
-import Class.Hotel
-import Class.Room
-import Class.User
-import Class.Visitor
